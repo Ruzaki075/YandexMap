7710270a8ce2bde14ff859a47b2e34047be59d55
Ruzaki075 <chernevigor78@gmail.com>
2025-11-13 14:17:45 +0300

    Добавлена яндекс карта с возможностью меток

---
+++ yandexMap/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import YandexMap from "./components/YandexMap";
+
+function App() {
+  return (
+    <div className="App">
+
+      <YandexMap />
+    </div>
+  );
+}
+
+export default App;
